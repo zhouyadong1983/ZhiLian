576b278bcf88613171dbfaff1014cbb6055a3db8
zhouyadong1983 <191337976@qq.com>
2020-09-20 09:48:27 +0800

    加入视频播放功能
    创建了StreamingAssets文件夹和里面的Video文件夹，Video文件夹放入两个视频，在教育三级界面可以播放/暂停这两个视频

---
+++ Assets/Scripts/三级场景/PlayVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    public VideoPlayer videoFst;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

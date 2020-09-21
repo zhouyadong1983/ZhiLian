80d52dff54e137efc80616211ec8548ce61679f4
zhouyadong1983 <191337976@qq.com>
2020-09-21 20:43:44 +0800

    视频播放路径将绝对路径改为相对路径
    播放StreamingAssets文件夹下Video文件夹下的两个视频

---
+++ Assets/Scripts/三级场景/PlayVideo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    private GameObject videoObject;
+    private VideoPlayer moviePlayerL;
+    private VideoPlayer moviePlayerR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moviePlayerL = GameObject.Find("Canvas/银医通/ImageL").transform.GetComponent<VideoPlayer>();
+        moviePlayerL.url = "file://"+Application.dataPath+ "/StreamingAssets/Video/L.mp4";
+
+        moviePlayerR = GameObject.Find("Canvas/银医通/ImageR").transform.GetComponent<VideoPlayer>();
+        moviePlayerR.url = "file://" + Application.dataPath + "/StreamingAssets/Video/R.mp4";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void  PlayLVideo()
+    {
+        moviePlayerL.Play();
+    }
+    public void PlayRVideo()
+    {
+        moviePlayerR.Play();
+    }
+}
